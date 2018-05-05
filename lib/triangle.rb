7b057af377fb33add06d5e79b9ba7abac67f1d86
joshpson <jpearson846@gmail.com>
2018-05-05 14:25:10 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    #binding.pry
+    if self.triangle_test
+      one_to_two = (@side_one == @side_two)
+      one_to_three = (@side_one == @side_three)
+      two_to_three = (@side_two == @side_three)
+      if (one_to_two && one_to_three)
+        :equilateral
+      elsif (one_to_two || one_to_three || two_to_three)
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      #begin
+        raise TriangleError
+      #rescue TriangleError => error
+      #    puts error.message
+      #end
+    end
+  end
+
+  def triangle_test
+   (@side_one > 0) && (@side_two > 0) &&
+   (@side_three > 0) && ((@side_one + @side_two) > @side_three) &&
+   ((@side_one + @side_three) > @side_two) &&
+   ((@side_two + @side_three) > @side_one)
+  end
+
+  class TriangleError < StandardError
+    def message
+      "That's not a triangle dummy!"
+    end
+  end
+
 end
